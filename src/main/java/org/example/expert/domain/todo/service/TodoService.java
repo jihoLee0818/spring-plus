4dffb9ccdaa6dee4079e52527bb1c475264af0c1
이지호 <leejiho0114@gmail.com>
2025-09-19 21:00:35 +0900

    perf(todo): Todo 단건 조회 QueryDSL 전환 및 N+1 문제 해결

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -79,7 +79,7 @@ public class TodoService {
     }
 
     public TodoResponse getTodo(long todoId) {
-        Todo todo = todoRepository.findByIdWithUser(todoId)
+        Todo todo = todoRepository.findByIdWithUserQuery(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         User user = todo.getUser();
